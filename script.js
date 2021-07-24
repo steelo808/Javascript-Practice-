20c0b29fa6d02c5756693e318719a56de67e8318
Sterling Newsom <sterlingnewsom808@gmail.com>
2021-07-24 10:31:13 -0700

    update 1.0

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 //love calculator
 // Create a love calculator that gives produces a random number based on two names
 
-// function loveCalculator(){
-//     prompt("what is your name");
-//     prompt("What is your spouses name");
+function loveCalculator(){
+    prompt("what is your name");
+    prompt("What is your spouses name");
 
-//     let n = Math.random();
-//     let compatibilty = Math.floor(n * 100) + 1;
+    let n = Math.random();
+    let compatibilty = Math.floor(n * 100) + 1;
 
-//     alert("Your compatibility percentage is " + compatibilty + "%");
+    alert("Your compatibility percentage is " + compatibilty + "%");
 
-// }
+}
 
 // loveCalculator();
 
@@ -20,54 +20,63 @@
 //BMI Calculator
 //Create a function that figures out your BMI and gives feedback based on the results
 
-// alert("Hello this is a program that will determine your bmi as well as if you're healthy or not. Select ok if you're ready");
-// let weight = prompt("How much do you weigh?");
-// let height = prompt("What is you height in inches?");
+alert("Hello this is a program that will determine your bmi as well as if you're healthy or not. Select ok if you're ready");
+let weight = prompt("How much do you weigh?");
+let height = prompt("What is you height in inches?");
 
-// function bmiCalculator (weight, height) {
+function bmiCalculator (weight, height) {
 
-//     let interpretation = 703 * weight/(height*height);
+    let interpretation = 703 * weight/(height*height);
 
-//     if (interpretation >= 25){
-//         console.log("Your BMI is " + interpretation + " Based on this data you are currently overweight");
-//     }
-//     else if (interpretation >= 18.5 && interpretation <= 24.9){
-//         console.log("Your BMI is " + interpretation + " Based on this data you are currently healthy");
-//     }
-//     else if (interpretation <= 24.9){
-//         console.log("Your BMI is " + interpretation + " Based on this data you are currently underweight")
-//     }
-//     else{
-//         console.log("Hmmm must be a mistake refresh the page and try inputing your numbers again");
-//     }
-//     return interpretation;
+    if (interpretation >= 25){
+        console.log("Your BMI is " + interpretation + " Based on this data you are currently overweight");
+    }
+    else if (interpretation >= 18.5 && interpretation <= 24.9){
+        console.log("Your BMI is " + interpretation + " Based on this data you are currently healthy");
+    }
+    else if (interpretation <= 24.9){
+        console.log("Your BMI is " + interpretation + " Based on this data you are currently underweight")
+    }
+    else{
+        console.log("Hmmm must be a mistake refresh the page and try inputing your numbers again");
+    }
+    return interpretation;
 
-// }
+}
 
 // bmiCalculator(weight, height);
 
 
 // Leap year challenge
+//create a function that determines what year is a leap year
 
-// let year = prompt("Enter a year to determine if its a leap year or not");
+let year = prompt("Enter a year to determine if its a leap year or not");
 
-// function isLeap(year) {
+function isLeap(year) {
 
-//     if(year % 4 === 0 && year % 400 === 0 && year % 100 !== 0){
-//         console.log("year " + year + " is a leap year");
-//     }
-//     else if (year % 4 === 0 && year % 100 !== 0){
-//         console.log("year " + year + " is a leap year");
-//     }
-//     else{
-//         console.log("year " + year + " is not a leap year");
-//     }
-//     }
+    if(year % 4 === 0 && year % 400 === 0 && year % 100 !== 0){
+        console.log("year " + year + " is a leap year");
+    }
+    else if (year % 4 === 0 && year % 100 !== 0){
+        console.log("year " + year + " is a leap year");
+    }
+    else{
+        console.log("year " + year + " is not a leap year");
+    }
+    }
 
-//     isLeap(year);
+
+
+    //isLeap(year);
 
 // Fizz Buzz Challenge
+// Write a short program that prints each number from 1 to 100 on a new line. 
 
+// For each multiple of 3, print "Fizz" instead of the number. 
+
+// For each multiple of 5, print "Buzz" instead of the number. 
+
+// For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.FizzBuzz
 
 let output = []; 
 let number = 1;
@@ -98,31 +107,7 @@ function fizzBuzz(){
 }
 
 
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-fizzBuzz();
-
+//fizzBuzz();
 
 
 
